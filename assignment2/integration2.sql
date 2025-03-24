b9cc12cf975fb652c5342e08d468aae3ea585aeb
Jon Han <jonhan2@illinois.edu>
2025-03-24 18:49:51 -0500

    change

--- assignment2/integration2.sql
+++ assignment2/integration2.sql
@@ -1,16 +1,14 @@
 INSERT INTO Integrated_Arrival
 
-
 SELECT a.Airline,
 a2.FlightNumber,
-a2.ScheduledArrivalDate
+a2.ScheduledArrivalDate,
 NULL AS ActualArrivalDate,
 a2.ScheduledArrivalTime,
 a2.ActualArrivalTime,
 a.GateTime,
 a.LandingTime,
 a.Terminal || a.Gate AS ArrivalGate
-
 
 FROM Airline2_Flight AS a2
 JOIN Airport3_Arrivals AS a ON a2.FlightNumber = a.FlightNumber;
